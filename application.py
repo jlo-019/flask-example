077fced270bce6113e33094c756131fc6ce8df2f
jlo-019 <79933443+jlo-019@users.noreply.github.com>
2024-04-05 21:38:35 +0100

    slight change to app.py

--- application.py
+++ application.py
@@ -1,12 +1,3 @@
-from flask import Flask, request, jsonify, render_template
-import torch
-import numpy as np
-from sentence_transformers import SentenceTransformer
-import pickle
-import boto3
-from io import BytesIO
-from torch import nn
-
 application = Flask(__name__)
 
 @application.route('/')
