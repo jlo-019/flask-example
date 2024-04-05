2db3d68e7d0ac96ed3bd13dc6fc873cd5372db40
jlo-019 <79933443+jlo-019@users.noreply.github.com>
2024-04-05 16:08:57 +0100

    removed amazon secret

--- application.py
+++ application.py
@@ -11,32 +11,6 @@ from torch import nn
 
 application = Flask(__name__)
 
-'''
-# Initialize Boto3 S3 client
-
-bucket_name = 'oritsejolomi-fyp'
-
-# Load the precomputed code embeddings and raw code snippets from S3 bucket
-code_embeddings = None
-combined_raw_code = None
-with BytesIO() as data:
-    s3_client.download_fileobj(bucket_name, 'code_embeddings.npy', data)
-    data.seek(0)
-    code_embeddings = np.load(data, allow_pickle=True)
-
-    s3_client.download_fileobj(bucket_name, 'combined_raw_code.pkl', data)
-    data.seek(0)
-    combined_raw_code = pickle.load(data)
-    # using s3_client.download_fileobj can be more memory-efficient since it allows to read contents 
-    # of S3 objects directly into memory without storing them on disk,
-
-# Load the pre-trained model
-model = SentenceTransformer('model_directory')
-
-#Define cosine_similarity to be accessible everywhere 
-cosine_similarity = nn.CosineSimilarity(dim=1)
-
-'''
 @application.route('/')
 def hello_world():
     return render_template('query.html')
