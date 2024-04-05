bf849287cb88f75b4cb0b489ddbf0df2b6aea08c
jlo-019 <79933443+jlo-019@users.noreply.github.com>
2024-04-05 21:10:16 +0100

    added imports in applicationpy and requirementstxt

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import torch
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import pickle
+import boto3
+import os
+from io import BytesIO
+from torch import nn
 
 application = Flask(__name__)
 
