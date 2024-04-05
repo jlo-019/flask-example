4dc37e0da8c38071331b6f8b90657ff0c6c13ac9
jlo-019 <79933443+jlo-019@users.noreply.github.com>
2024-04-05 16:36:10 +0100

    a=taken all out

--- application.py
+++ application.py
@@ -1,16 +1,7 @@
 from flask import Flask
-from flask import Flask, request, jsonify, render_template
-import torch
-import numpy as np
-from sentence_transformers import SentenceTransformer
-import pickle
-import boto3
-import os
-from io import BytesIO
-from torch import nn
 
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return render_template('query.html')
+    return 'CODE SEARCHX. HELLO THERE GOOD SIR.'
