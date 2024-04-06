103b446bfb67820c963d4f897646790cefd5aa53
jlo-019 <79933443+jlo-019@users.noreply.github.com>
2024-04-06 11:33:24 +0100

    new additions with static, etc...

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
+from flask import Flask, request, jsonify, render_template
+import torch
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import pickle
+import boto3
+import os
+from io import BytesIO
+from torch import nn
+
 application = Flask(__name__)
 
 @application.route('/')
